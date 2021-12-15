8b7d06a6adc3a70ae8e081c865b09f6282ca4d42
Edson Hirano <edson.hriano28@gmail.com>
2021-12-15 20:12:23 -0300

    Adicionando mat-toolbar principal + router-outlet

--- projeto/src/app/app.module.ts
+++ projeto/src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +15,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    RouterModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
